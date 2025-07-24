65a83b3ceae966090a2732b172534c87432c841b
Anshuman-X <anshumanacharya51@gmail.com>
2025-07-24 15:13:21 +0530

    used if-else to find the month name

---
+++ 01_cpp_basics/month.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int month;
+    cout<<"Enter the number: ";
+    cin>>month;
+    if(month==1)
+    {
+    cout<<"January";
+    }
+    else if(month==2)
+    {
+    cout<<"february";
+    }
+    else if(month==3)
+    {
+    cout<<"march";
+    }
+    else if(month==4)
+    {
+    cout<<"april";
+    }
+    else if(month==5)
+    {
+    cout<<"may";
+    }
+    else if(month==6)
+    {
+    cout<<"june";
+    }
+    else if(month==7)
+    {
+    cout<<"july";
+    }
+    else if(month==8)
+    {
+    cout<<"august";
+    }
+    else if(month==9)
+    {
+    cout<<"september";
+    }
+    else if(month==10)
+    {
+    cout<<"october";
+    }
+    else if(month==11)
+    {
+    cout<<"november";
+    }
+    else if(month==12)
+    {
+    cout<<"december";
+    }
+    else{
+    cout<<"Enter a number between 1 to 12";
+    }
+}
