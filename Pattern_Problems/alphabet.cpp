73bdcce261a06b69bd813462fd20a6a889236a48
Anshuman-X <anshumanacharya51@gmail.com>
2025-07-26 23:58:02 +0530

    To print a to e

---
+++ Pattern_Problems/alphabet.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name='a';
+    int row,col;
+
+    for(row=1;row<=5;row=row+1)
+    {
+        char name='a'+(row-1);
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<<name<<" ";
+    
+        }
+        cout<<endl;
+    }
+}
