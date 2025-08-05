415b01e2af1142aca99e3764eea9092566e1c729
Anshuman-X <anshumanacharya51@gmail.com>
2025-08-05 16:53:15 +0530

    To print the AP of difference 7 from 220 to 730

---
+++ 01_cpp_basics/arithmatic_progression.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=220,d=7;
+    for(int term=a;term<=730;term=term+d)
+    {
+        cout<<term<<" ";
+    }
+    return 0;
+    
+}
