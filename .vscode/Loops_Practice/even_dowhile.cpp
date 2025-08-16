62d0e9ac7e2147a5f3d39e71be98b1dec96a4fc8
Anshuman-X <anshumanacharya51@gmail.com>
2025-08-16 23:24:09 +0530

    Added a new folder named Loops_Practice to practice questions using loops

---
+++ .vscode/Loops_Practice/even_dowhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int n;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int i=1;
+    do{
+        if(i%2==0)
+        cout<<i<<" ";
+
+        i++;
+    }while(i<=n);
+}
