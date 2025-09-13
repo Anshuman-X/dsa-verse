fcb91393afd6b9679eabe62efb9ad814b6f05b71
Anshuman-X <anshumanacharya51@gmail.com>
2025-09-13 23:18:50 +0530

    To move all zeroes to the end of an array

---
+++ ARRAY/Moving_Zeros.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[20];
+
+    // int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the numbers: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int index = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            swap(arr[i], arr[index]);
+            index++;
+        }
+    }
+    cout << "After Moving Zeroes: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
