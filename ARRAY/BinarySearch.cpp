2958f3367607cf2439c4b512c0bfe86ba56c7512
Anshuman-X <anshumanacharya51@gmail.com>
2025-09-22 23:30:39 +0530

    Added binary search program.

---
+++ ARRAY/BinarySearch.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int n, int key)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key)
+        {
+            cout << "Found at index ";
+            return mid;
+        }
+        else if (arr[mid] < key)
+            start = mid + 1;
+
+        else
+            end = mid - 1;
+    }
+    return -1;
+}
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cout << "Enter the key: ";
+    cin >> key;
+
+    cout << BinarySearch(arr, n, key);
+    return 0;
+}
