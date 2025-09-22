c2643a387691072ad66acbf48030b03ae5e3ac6e
Anshuman-X <anshumanacharya51@gmail.com>
2025-09-23 00:02:21 +0530

    Added binary search program to find index of the element in a decreasing sorted array.

---
+++ ARRAY/DecreasingBS.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int BinarySearch(int arr[], int n, int key)
+{
+
+    // 50 40 30 20 10
+    int start = 0;
+    int end = n - 1;
+    int mid;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+
+        if (arr[mid] == key)
+        {
+            cout << "Found at index ";
+            return mid;
+        }
+        else if (arr[mid] < key)
+            end = mid - 1;
+
+        else
+            start = mid + 1;
+    }
+    return -1;
+}
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int key;
+    cout << "Enter the key: ";
+    cin >> key;
+
+    cout << BinarySearch(arr, n, key);
+
+    return 0;
+}
